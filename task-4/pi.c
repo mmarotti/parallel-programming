a899a80d7095a3aac17e812a2d2f20f55144eb03
Matheus Marotti <matheus@bgcbrasil.com.br>
2021-07-12 08:23:11 -0300

    Added task-4, pi calculus

---
+++ task-4/pi.c
@@ -0,0 +1,33 @@
+#include <omp.h>
+#include <stdio.h>
+
+static long num_steps = 100000;
+double step;
+
+#define NMAX 50 // max de threads
+#define NUM_THREADS 2
+
+void main () {
+  int i; double x, pi, sum[NMAX];
+  int nthreads;
+
+  step = 1.0/(double) num_steps;
+  omp_set_num_threads (NUM_THREADS);
+
+  #pragma omp parallel
+  {
+    int i, id, nthrds;
+    id = omp_get_thread_num();
+    nthrds = omp_get_num_threads();
+    if (id == 0) nthreads = nthrds;
+    for (i=id, sum[id]=0.0; i< num_steps; i=i+nthrds){
+      x = (i+0.5)*step;
+      sum[id]+= 4.0/(1.0+x*x);
+    }
+
+  }
+
+  for(i=0,pi=0.0; i<nthreads;i++) pi += sum[i]*step;
+
+  printf("Valor de pi %f", pi);
+}
